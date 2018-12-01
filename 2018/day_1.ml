5d63c68bbf3881a8a605f3dc23c1d87cfdec947e
mackenziestarr <mackestarr@gmail.com>
2018-12-01 18:34:45 -0500

    day one, part two in which i learn that processing a file stream multiple times is hard and eventually settle for a gross, in-memory solution

--- 2018/day_1.ml
+++ 2018/day_1.ml
@@ -1,20 +1,42 @@
+module Int = struct
+   type t = int
+   (* use Pervasives compare *)
+   let compare = compare
+end
+
+module Ints = Set.Make(Int)
+
 let input_file = "./input/day_1_input.txt"
 
-let read channel parse_fn =
+let make_stream channel =
   Stream.from
-    (fun _ -> try Some (parse_fn channel) with End_of_file -> None)
+    (fun _ -> try Some (int_of_string (input_line channel)) with End_of_file -> None)
 
-let stream_fold f stream init =
-  let result = ref init in
-  Stream.iter
-    (fun x -> result := f x !result)
-    stream;
-    !result;;
-
-let main () =
-  let in_channel = open_in input_file in
-  let ints       = read in_channel (fun chan -> (int_of_string (input_line chan)))  in
+let part_one () =
+  let stream_fold f stream init =
+    let result = ref init in
+    Stream.iter (fun x -> result := f x !result) stream;
+    !result in
+  let ints       = make_stream (open_in input_file) in
   let result     = stream_fold (+) ints 0 in
-  Printf.printf "result: %d\n" result
+  Printf.printf "[1.1] = %d\n" result
 
-let () = main ()
+let part_two () =
+  let rec find array index length acc set =
+    let value = ((+) acc (Array.get array index)) in
+    if (Ints.mem value set) then value
+    else find array ((mod) ((+) index 1) length) length value (Ints.add value set)
+  in
+  let list_of_stream stream =
+    let result = ref [] in
+    Stream.iter (fun value -> result := value :: !result) stream;
+    List.rev !result
+  in
+  let ints   = Array.of_list (list_of_stream (make_stream (open_in input_file))) in
+  let result = find ints 0 (Array.length ints) 0 Ints.empty in
+  Printf.printf "[1.2] = %d\n" result
+
+let () =
+  print_endline "* Day 1 *";
+  part_one ();
+  part_two ()
