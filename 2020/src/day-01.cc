364a78f604ae63e6e68ae7803337b9b89f042b64
mackenziestarr <mackestarr@gmail.com>
2020-12-01 09:57:04 -0500

    2020: day 1, part 2 :star:

--- 2020/src/day-01.cc
+++ 2020/src/day-01.cc
@@ -4,6 +4,8 @@
 #include <string>
 #include <fstream>
 #include <iostream>
+#include <set>
+#include <numeric>
 
 std::vector<int> parse(const std::string& input_file) {
   std::ifstream in(input_file);
@@ -16,17 +18,44 @@ std::vector<int> parse(const std::string& input_file) {
   };
 }
 
-std::pair<int, int> two_sum(const std::vector<int>& input, const int target) {
-  for (const auto i : input) {
-    for (const auto j : input) {
-      if (i + j == target) return {i, j};
-    }
+void combine(std::vector<int>& input, int offset, int k, std::vector<int>& combo, std::vector<std::vector<int>>& out) {
+  if (k == 0) {
+    out.push_back(combo);
+    return;
   }
-  return {0, 0};
+  for (int i = offset; i <= input.size() - k; i++) {
+    combo.push_back(input[i]);
+    combine(input, i + 1, k - 1, combo, out);
+    combo.pop_back();
+  }
+}
+
+std::vector<std::vector<int>> combine(std::vector<int>& input, int k) {
+  std::vector<std::vector<int>> out;
+  std::vector<int> combo;  
+  combine(input, 0, k, combo, out);
+  return out;
+}
+
+std::vector<int> combination_summing_to_n(std::vector<int>& input, const int k, const int n) {
+  std::vector<std::vector<int>> combinations = combine(input, k);
+  for (const auto& c : combinations) {
+    if (std::accumulate(c.begin(), c.end(), 0) == n) return c;
+  }
+  return {};
 }
 
 TEST_CASE() {
   std::vector<int> input = parse("src/day-01.input");
-  std::pair<int, int> numbers = two_sum(input, 2020);
-  REQUIRE(numbers.first * numbers.second == 787776);
+  std::vector<int> numbers = combination_summing_to_n(input, 2, 2020);
+  int answer = std::accumulate(numbers.begin(), numbers.end(), 1, std::multiplies<int>());
+  REQUIRE(answer == 787776);
 }
+
+TEST_CASE() {
+  std::vector<int> input = parse("src/day-01.input");
+  std::vector<int> numbers = combination_summing_to_n(input, 3, 2020);
+  int answer = std::accumulate(numbers.begin(), numbers.end(), 1, std::multiplies<int>());
+  REQUIRE(answer == 262738554);
+}
+
