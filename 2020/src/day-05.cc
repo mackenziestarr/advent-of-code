d44f7a7c542f4c61562a91ff3986cc18acfc400a
mackenziestarr <mackestarr@gmail.com>
2020-12-07 00:41:19 -0500

    2020: day 5, part 1 :zzz:

---
+++ 2020/src/day-05.cc
@@ -0,0 +1,43 @@
+#include <catch2/catch_test_macros.hpp>
+#include <utility>
+#include <fstream>
+#include <numeric>
+#include <iostream>
+
+int decode(const std::string_view& input) {
+  std::string_view row_tokens = input.substr(0, 7);
+  std::string_view col_tokens = input.substr(7);
+
+  int low = 0, high = 127, mid;
+  for (const auto& t : row_tokens) {
+    mid = (high + low) / 2;
+    if (t == 'F') high = mid;
+    if (t == 'B') low = mid + 1;
+  }
+  int row = low;
+
+  low = 0, high = 7;
+  for (const auto& t : col_tokens) {
+    mid = (high + low) / 2;
+    if (t == 'L') high = mid;
+    if (t == 'R') low = mid + 1;
+  }
+  int col = low;
+
+  return (row * 8) + col;
+}
+
+TEST_CASE("part one") {
+  std::ifstream in("src/day-05.input");
+  if (!in) throw std::runtime_error("couldn't open file");
+
+  int answer =
+    std::transform_reduce(
+      (std::istream_iterator<std::string>(in)),
+      std::istream_iterator<std::string>(),
+      int{0},
+      [](int lhs, int rhs) { return std::max(lhs, rhs); },
+      [](const std::string_view& s) { return decode(s); });
+  
+  REQUIRE(answer == 998);
+}
