fba6170ddc133a1bb72b25916a221675d42236e2
mackenziestarr <mackestarr@gmail.com>
2020-12-24 11:04:46 -0500

    2020: day 12, part 2 :christmas_tree:

--- 2020/src/day-12.cc
+++ 2020/src/day-12.cc
@@ -3,6 +3,7 @@
 #include <vector>
 #include <sstream>
 #include <fstream>
+#include <unordered_map>
 #include "lib.h"
 
 struct Coordinate {
@@ -12,7 +13,16 @@ struct Coordinate {
     y += rhs.y;
     return *this;
   }
+  Coordinate& operator*=(const Coordinate& rhs) {
+    x *= rhs.x;
+    y *= rhs.y;
+    return *this;
+  }
 };
+
+Coordinate operator*(Coordinate lhs, const Coordinate& rhs) {
+  return lhs *= rhs;
+}
 
 constexpr auto parse = [](auto&& in) {
   return aoc::parse(in, [](const std::string& line) -> std::pair<char, int> {
@@ -47,6 +57,53 @@ namespace part_one {
   }
 }
 
+namespace part_two {
+  auto navigate(auto steps, auto waypoint, auto ship) {
+    for (auto& [action, amount] : steps) {
+      if (action == 'F') {
+	ship += (waypoint * Coordinate {amount, amount});
+      }
+      switch (action) {
+      case 'N': { waypoint += {0, +amount}; break; }
+      case 'S': { waypoint += {0, -amount}; break; }
+      case 'E': { waypoint += {-amount, 0}; break; }
+      case 'W': { waypoint += {+amount, 0}; break; }
+      case 'L': {
+	std::unordered_map<int, std::pair<Coordinate, bool>>
+        ccw_rotations = {
+	    {90,  {{+1, -1}, true }},
+	    {180, {{-1, -1}, false}},
+	    {270, {{-1, +1}, true }}
+       };
+       const auto& [flip_signs, swap_coordinates] = ccw_rotations[amount];
+       if (swap_coordinates) {
+	  auto& [x, y] = waypoint;
+	  std::swap(x, y);
+       }
+       waypoint *= flip_signs;
+       break;
+      }
+      case 'R': {
+	std::unordered_map<int, std::pair<Coordinate, bool>>
+        cw_rotations = {
+	    {90,  {{-1, +1}, true }},
+	    {180, {{-1, -1}, false}},
+	    {270, {{+1, -1}, true }}
+       };
+       const auto& [flip_signs, swap_coordinates] = cw_rotations[amount];
+       if (swap_coordinates) {
+	  auto& [x, y] = waypoint;
+	  std::swap(x, y);
+       }
+       waypoint *= flip_signs;
+       break;
+      }
+      }
+    }
+    return ship;
+  }
+}
+
 auto manhattan_distance(const auto& from, const auto& to) {
   return std::abs(from.x - to.x)
        + std::abs(from.y - to.y);
@@ -63,12 +120,25 @@ F7
 R90
 F11)"
     });
-    const auto& destination = part_one::navigate(input, origin);
-    REQUIRE(manhattan_distance(destination, origin) == 25);
+    {
+      const auto& destination = part_one::navigate(input, origin);
+      REQUIRE(manhattan_distance(destination, origin) == 25);
+    }
+    {
+      Coordinate waypoint {-10, 1};
+      const auto& destination = part_two::navigate(input, waypoint, origin);
+      REQUIRE(manhattan_distance(destination, origin) == 286);
+    }
   }
   SECTION("part one") {
     auto input = parse(std::ifstream { "input/day-12.input"});
     const auto& destination = part_one::navigate(input, origin);
+    REQUIRE(manhattan_distance(destination, origin) == 439);
+  }
+  SECTION("part two") {
+    Coordinate waypoint {-10, 1};    
+    auto input = parse(std::ifstream { "input/day-12.input"});
+    const auto& destination = part_two::navigate(input, waypoint, origin);
     REQUIRE(manhattan_distance(destination, origin) == 439);
   } 
 }
