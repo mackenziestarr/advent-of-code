d7ce84443da83855ab2d9fd4b37a7d1d0228839b
mackenziestarr <mackestarr@gmail.com>
2021-01-16 17:40:42 -0500

    2020: day 17, part 2 :rat:

--- 2020/src/day-17.cc
+++ 2020/src/day-17.cc
@@ -6,22 +6,24 @@
 #include <unordered_set>
 #include <algorithm>
 
-struct vec3 {
-  int x, y, z;
+struct vec4 {
+  int x, y, z, w;
+  bool use_4th_dimension = true;
   
   auto adjacents() const {
-    std::vector<vec3> adjacents;
+    std::vector<vec4> adjacents;
     for (int x_offset : {-1, 0, 1}) {
       for (int y_offset : {-1, 0, 1}) {
 	for (int z_offset : {-1, 0, 1}) {
-	  if (x_offset == 0 && y_offset == 0 && z_offset == 0) {
-	    continue;
+	  for (int w_offset : {-1, 0, 1}) {
+	    if (x_offset == 0 && y_offset == 0 && z_offset == 0 && w_offset == 0) {
+	      continue;
+	    }
+	    if (!use_4th_dimension && w_offset != 0) continue;
+	    vec4 adjacent { x + x_offset, y + y_offset, z + z_offset, w + w_offset };
+	    adjacent.use_4th_dimension = use_4th_dimension;
+	    adjacents.push_back(adjacent);
 	  }
-	  adjacents.push_back({
-	      x + x_offset,
-	      y + y_offset,
-	      z + z_offset
-	  });
 	}
       }
     }
@@ -34,33 +36,33 @@ struct vec3 {
     return result;
   }
 
-  bool operator==(const vec3& rhs) const {
-    return x == rhs.x && y == rhs.y && z == rhs.z;
+  bool operator==(const vec4& rhs) const {
+    return x == rhs.x && y == rhs.y && z == rhs.z && w == rhs.w;
   }
 
   struct hash_fn {
-    std::size_t operator()(const vec3& v) const {
+    std::size_t operator()(const vec4& v) const {
       return std::hash<int>()(v.x)
            ^ std::hash<int>()(v.y) << 1
-      	   ^ std::hash<int>()(v.z) << 2;
+      	   ^ std::hash<int>()(v.z) << 2
+      	   ^ std::hash<int>()(v.w) << 3;
     }
   };
   
 };
 
-std::ostream& operator<<(std::ostream& os, const vec3& v) {
-  os << '{' << v.x << ',' << v.y << ',' << v.z << '}';
-  return os;
-}
-
-constexpr auto parse = [](auto&& in) {
-  std::unordered_set<vec3, vec3::hash_fn> out;
+constexpr auto parse = [](auto&& in, bool use_4th_dimension = true) {
+  std::unordered_set<vec4, vec4::hash_fn> out;
   std::string line;
   int y = 0;
   while (std::getline(in, line)) {
     int x = 0;
     for (auto c : line) {
-      if (c == '#') out.insert({x, y, 0});
+      if (c == '#') {
+	vec4 v = {x, y, 0, 0};
+	v.use_4th_dimension = use_4th_dimension;
+	out.insert(v);
+      }
       x++;
     }
     y++;
@@ -68,14 +70,14 @@ constexpr auto parse = [](auto&& in) {
   return out;
 };
 
-bool is_next_active(const auto& vec3, const auto& set) {
+bool is_next_active(const auto& vec4, const auto& set) {
   std::size_t active_neighbors = std::ranges::count_if(
-    vec3.adjacents(),
+    vec4.adjacents(),
     [&set](const auto& adjacent) {
       return set.contains(adjacent);
     }
   );
-  if (set.contains(vec3)) {
+  if (set.contains(vec4)) {
     return active_neighbors == 2
         || active_neighbors == 3;
   }
@@ -83,7 +85,7 @@ bool is_next_active(const auto& vec3, const auto& set) {
 }
 
 auto simulate(auto input) {
-  std::unordered_set<vec3, vec3::hash_fn> next_state;
+  std::unordered_set<vec4, vec4::hash_fn> next_state;
   for (const auto& active : input) {
     for (const auto& coord : active.adjacents_with_self()) {
       if (is_next_active(coord, input)) {
@@ -95,19 +97,34 @@ auto simulate(auto input) {
 }
 
 TEST_CASE("day seventeen") {
-  SECTION("example input") {
-    std::stringstream example_input { ".#.\n..#\n###" };
-    auto map = parse(example_input);
+  SECTION("example input, part one") {
+    auto map = parse(std::stringstream { ".#.\n..#\n###" }, false /* use_4th_dimension */);
     for (int cycles = 0; cycles < 6; cycles++) {
       map = simulate(map);
     }
     REQUIRE(map.size() == 112);
   }
   SECTION("part one") {
-    auto map = parse(std::ifstream{"input/day-17.input"});
+    auto map = parse(std::ifstream{"input/day-17.input"}, false /* use_4th_dimension */);
     for (int cycles = 0; cycles < 6; cycles++) {
       map = simulate(map);
     }
     REQUIRE(map.size() == 295);
+  }
+  SECTION("example input, part two") {
+    std::stringstream example_input { ".#.\n..#\n###" };
+    auto map = parse(example_input, true /* use_4th_dimension */);
+    for (int cycles = 0; cycles < 6; cycles++) {
+      map = simulate(map);
+    }
+    REQUIRE(map.size() == 848);
+  }
+  SECTION("part two") {
+    std::stringstream example_input { ".#.\n..#\n###" };
+    auto map = parse(std::ifstream{"input/day-17.input"}, true /* use_4th_dimension */);    
+    for (int cycles = 0; cycles < 6; cycles++) {
+      map = simulate(map);
+    }
+    REQUIRE(map.size() == 848);
   }
 }
