189b6126fd32daea7d4e44e9193fffd4485622e5
mackenziestarr <mackestarr@gmail.com>
2021-12-06 10:24:58 -0500

    day two, part two :star:

--- 2021/src/day-02.rs
+++ 2021/src/day-02.rs
@@ -1,11 +1,13 @@
 #[derive(Debug)]
 struct Answer {
+    aim: i32,
     position: i32,
     depth: i32,
 }
 
 fn part_one(input: &Vec<(&str, i32)>) -> i32 {
     let init = Answer {
+        aim: 0,
         position: 0,
         depth: 0,
     };
@@ -27,6 +29,44 @@ fn part_one(input: &Vec<(&str, i32)>) -> i32 {
     result.depth * result.position
 }
 
+fn part_two(input: &Vec<(&str, i32)>) -> i32 {
+    let init = Answer {
+        aim: 0,
+        position: 0,
+        depth: 0,
+    };
+    let result = input.iter().fold(init, |result, s| match s {
+        ("up", x) => Answer {
+            aim: result.aim - x,
+            ..result
+        },
+        ("down", x) => Answer {
+            aim: result.aim + x,
+            ..result
+        },
+        ("forward", x) => Answer {
+            position: result.position + x,
+            depth: result.depth + (result.aim * x),
+            ..result
+        },
+        _ => result,
+    });
+    result.depth * result.position
+}
+
+#[test]
+fn test_part_two() {
+    let input = [
+        ("forward", 5),
+        ("down", 5),
+        ("forward", 8),
+        ("up", 3),
+        ("down", 8),
+        ("forward", 2),
+    ];
+    assert_eq!(part_two(&input.to_vec()), 900)
+}
+
 fn main() {
     let input: Vec<(&str, i32)> = include_str!("../input/day-02.txt")
         .lines()
@@ -38,5 +78,5 @@ fn main() {
 
     println!("day two:");
     println!("- part one: {}", part_one(&input));
-    println!("- part two: {}", "TODO");
+    println!("- part two: {}", part_two(&input));
 }
